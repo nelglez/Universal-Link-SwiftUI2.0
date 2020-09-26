ed83a756d30e0430a61888e07230d18b2ffd89d5
Nelson Gonzalez <nelglezfl@gmail.com>
2020-09-26 12:49:18 -0400

    Initial Commit

---
+++ Universal-Link-SwiftUI2.0/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Universal-Link-SwiftUI2.0
+//
+//  Created by Nelson Gonzalez on 9/26/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Universal-Link-SwiftUI2.0/Universal_Link_SwiftUI2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Universal_Link_SwiftUI2_0App.swift
+//  Universal-Link-SwiftUI2.0
+//
+//  Created by Nelson Gonzalez on 9/26/20.
+//
+
+import SwiftUI
+
+@main
+struct Universal_Link_SwiftUI2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
